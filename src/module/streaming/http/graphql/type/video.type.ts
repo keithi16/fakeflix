ae35aa6493bdb8805709ab64ec7bb8385dcdc157
Waldemar Neto <waldemarnt@gmail.com>
2023-07-01 18:43:13 -0300

    Remove unncessary fields

--- src/module/streaming/http/graphql/type/video.type.ts
+++ src/module/streaming/http/graphql/type/video.type.ts
@@ -2,18 +2,6 @@ import { Directive, Field, ID, ObjectType } from '@nestjs/graphql';
 
 @ObjectType({ description: 'video ' })
 export class Video {
-  @Field(() => ID)
-  id: string;
-
   @Directive('@upper')
   title: string;
-
-  @Field({ nullable: true })
-  description?: string;
-
-  @Field()
-  creationDate: Date;
-
-  @Field(() => [String])
-  ingredients: string[];
 }
