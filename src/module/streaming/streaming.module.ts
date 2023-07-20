9626a718a36626b84208cf3153309e19ce69ced9
William Calderipe <wcalderipe@gmail.com>
2023-07-20 18:10:41 +0200

    Sort imports on format (#9)

--- src/module/streaming/streaming.module.ts
+++ src/module/streaming/streaming.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { VideoResolver } from './http/graphql/video.resolver';
 import { DatabaseModule } from '@src/shared/module/database/database.module';
-import { VideoUploadService } from './domain/service/video-upload.service';
 import { VideoRepository } from './domain/repository/video.repository.interface';
+import { VideoUploadService } from './domain/service/video-upload.service';
+import { VideoResolver } from './http/graphql/video.resolver';
 import { VideoPrismaRepository } from './storage/repository/video.prisma-repository';
 
 @Module({
