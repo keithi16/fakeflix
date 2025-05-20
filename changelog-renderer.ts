c19f266a1438b5e95fbcd455ea10f3fff54bfcf8
Waldemar Neto <waldemarnt@gmail.com>
2025-05-20 18:39:29 -0300

    chore(release): publish
    - project: billing-api 2.4.0

---
+++ changelog-renderer.ts
@@ -0,0 +1,52 @@
+import { execSync } from 'child_process';
+import * as fs from 'fs';
+import DefaultChangelogRenderer from 'nx/release/changelog-renderer';
+import { ChangelogChange } from 'nx/src/command-line/release/changelog';
+
+export default class MyChangelogRenderer extends DefaultChangelogRenderer {
+  protected filterChanges(
+    changes: ChangelogChange[],
+    project: string | null
+  ): ChangelogChange[] {
+    const graphCacheFileName = `.nx-cache.graph.${project}.output.json`;
+    /**
+     * Gets the full graph of the project and its dependencies
+     */
+    const output = execSync(
+      `nx graph --focus ${project} --open=false --file=${graphCacheFileName}`,
+      {
+        encoding: 'utf-8',
+      }
+    );
+    console.log('NX Graph temp file created:', output);
+    const graph = fs.readFileSync(graphCacheFileName, 'utf-8');
+    const parsedGraph = JSON.parse(graph);
+    console.log('Related projects:', Object.keys(parsedGraph.graph.dependencies));
+    const relevantProjects = Object.keys(parsedGraph.graph.dependencies);
+
+    const affectedProjects = new Set<string>();
+    for (const change of changes) {
+      if (change.affectedProjects === '*') {
+        relevantProjects.forEach((project) => affectedProjects.add(project));
+        continue;
+      }
+      const afftectedProject = change.affectedProjects.find((project) =>
+        relevantProjects.includes(project)
+      );
+      if (afftectedProject) {
+        affectedProjects.add(afftectedProject);
+      }
+    }
+    console.log('Affected projects:', Array.from(affectedProjects));
+
+    const filteredChanges = changes.filter((change) => {
+      if (change.affectedProjects === '*') return true;
+
+      return change.affectedProjects.some((project) =>
+        relevantProjects.includes(project)
+      );
+    });
+    fs.unlinkSync(graphCacheFileName);
+    return filteredChanges;
+  }
+}
