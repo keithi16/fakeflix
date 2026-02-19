cf04be5bbcb96219dbb0c1c154c26b3d129e4b93
Waldemar Neto <waldemarnt@gmail.com>
2026-02-19 19:54:10 -0300

    chore: remove wrong export

--- package/billing/billing.module.ts
+++ package/billing/billing.module.ts
@@ -1,28 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '@tlc/shared-module/auth';
+import { LoggerModule } from '@tlc/shared-module/logger';
 import { ClsModule } from 'nestjs-cls';
-import { SubscriptionService } from './core/service/subscription.service';
-import { SubscriptionBillingService } from './core/service/subscription-billing.service';
-import { ProrationCalculatorService } from './core/service/proration-calculator.service';
-import { UsageBillingService } from './core/service/usage-billing.service';
-import { TaxCalculatorService } from './core/service/tax-calculator.service';
+import { AddOnManagerService } from './core/service/add-on-manager.service';
+import { CreditManagerService } from './core/service/credit-manager.service';
 import { DiscountEngineService } from './core/service/discount-engine.service';
+import { DunningManagerService } from './core/service/dunning-manager.service';
 import { InvoiceGeneratorService } from './core/service/invoice-generator.service';
 import { InvoiceService } from './core/service/invoice.service';
-import { CreditManagerService } from './core/service/credit-manager.service';
-import { AddOnManagerService } from './core/service/add-on-manager.service';
-import { DunningManagerService } from './core/service/dunning-manager.service';
-import { SubscriptionController } from './http/rest/controller/subscription.controller';
-import { SubscriptionBillingController } from './http/rest/controller/subscription-billing.controller';
-import { InvoiceController } from './http/rest/controller/invoice.controller';
-import { UsageController } from './http/rest/controller/usage.controller';
-import { CreditController } from './http/rest/controller/credit.controller';
-import { BillingFacade } from './public-api/facade/billing.facade';
+import { ProrationCalculatorService } from './core/service/proration-calculator.service';
+import { SubscriptionBillingService } from './core/service/subscription-billing.service';
+import { SubscriptionService } from './core/service/subscription.service';
+import { TaxCalculatorService } from './core/service/tax-calculator.service';
+import { UsageBillingService } from './core/service/usage-billing.service';
+import { AccountingIntegrationClient } from './http/client/accounting-api/accounting-integration.client';
 import { EasyTaxClient } from './http/client/easytax-api/easytax-tax.client';
 import { PaymentGatewayClient } from './http/client/payment-gateway-api/payment-gateway.client';
-import { AccountingIntegrationClient } from './http/client/accounting-api/accounting-integration.client';
+import { CreditController } from './http/rest/controller/credit.controller';
+import { InvoiceController } from './http/rest/controller/invoice.controller';
+import { SubscriptionBillingController } from './http/rest/controller/subscription-billing.controller';
+import { SubscriptionController } from './http/rest/controller/subscription.controller';
+import { UsageController } from './http/rest/controller/usage.controller';
 import { BillingPersistenceModule } from './persistence/billing-persistence.module';
-import { LoggerModule } from '@tlc/shared-module/logger';
+import { BillingFacade } from './public-api/facade/billing.facade';
 
 const coreServices = [
   SubscriptionService,
@@ -38,15 +38,9 @@ const coreServices = [
   DunningManagerService,
 ];
 
-const httpClients = [
-  EasyTaxClient,
-  PaymentGatewayClient,
-  AccountingIntegrationClient,
-];
+const httpClients = [EasyTaxClient, PaymentGatewayClient, AccountingIntegrationClient];
 
-const publicApiFacades = [
-  BillingFacade,
-];
+const publicApiFacades = [BillingFacade];
 
 @Module({
   imports: [
@@ -66,7 +60,7 @@ const publicApiFacades = [
     UsageController,
     CreditController,
   ],
-  exports: [BillingFacade, ...coreServices],
+  exports: [BillingFacade],
 })
 export class BillingModule {}
 
