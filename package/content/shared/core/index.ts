0a0f4b36a78319b4af1620372909b6aaf93d9379
Waldemar Neto <waldemarnt@gmail.com>
2026-03-24 17:13:52 -0300

    chore: fix tests

--- package/content/shared/core/index.ts
+++ package/content/shared/core/index.ts
@@ -1,5 +1,5 @@
 export * from './guard/content-type.guard';
 export * from './enum/content-type.enum';
 export * from './exception/video-not-found.exception';
-export { Content, MovieContent, TvShowContent } from '../../persistence/entity/content.entity';
+export { Content, MovieContent, TvShowContent } from '../persistence/entity/content.entity';
 
