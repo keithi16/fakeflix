95c30e3587e6cabf1829029562cdc350e8a075ae
Waldemar Neto <waldemarnt@gmail.com>
2026-05-05 16:43:29 -0300

    chore: fix billing api bootstrap

--- app/billing-api/main.ts
+++ app/billing-api/main.ts
@@ -2,11 +2,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@tlc/shared-module/config';
 import { LoggerFactory } from '@tlc/shared-module/logger';
+import { initializeTransactionalContext } from 'typeorm-transactional';
 import { BillingApiModule } from './billing-api.module';
 import { BillingApiConfig } from './config';
 
 async function bootstrap() {
   const logger = LoggerFactory('billing-api');
+  initializeTransactionalContext();
   const app = await NestFactory.create(BillingApiModule, { bufferLogs: true });
   const configService = app.get<ConfigService<BillingApiConfig>>(ConfigService);
   const port = configService.get('billingApi.port');
